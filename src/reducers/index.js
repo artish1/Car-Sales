a40b5dfceb6db7cee7b76f61abcbdc3208683df7
Mark Artishuk <markyshuk@gmail.com>
2019-11-06 11:47:09 -0800

    setup redux store and wrapped app in provider

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+export const initialState = {};
+
+export const reducer = (state = initialState, action) => {
+  console.log("Reducerrrr");
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
