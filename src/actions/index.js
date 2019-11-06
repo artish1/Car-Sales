e5fc280f3883fe85a19db97464c57b71f41d2e22
Mark Artishuk <markyshuk@gmail.com>
2019-11-06 11:56:54 -0800

    Added some action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 //Action Types
+export const ADD_FEATURE = "ADD_FEATURE";
+export const REMOVE_FEATURE = "REMOVE_FEATURE";
 
 //Action creators
+
+export const addFeature = id => ({ type: ADD_FEATURE, payload: id });
+export const removeFeature = id => ({ type: REMOVE_FEATURE, payload: id });
